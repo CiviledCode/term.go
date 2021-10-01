56d330a3d0fb08f5dd43cb89003b85b4d47b0621
CiviledCode <civiled@usa.com>
2021-10-01 14:07:27 -0700

    - Updated ClearScreen escape code.

--- screen/screen.go
+++ screen/screen.go
@@ -36,7 +36,7 @@ func NewScreen() *Screen {
 
 // ClearScreen sends an ASCII escape character to the Screen writer to clear the terminal history.
 func (s *Screen) ClearScreen() {
-	fmt.Fprint(s.Terminal, "\\033c")
+	fmt.Fprint(s.Terminal, "\\\033c")
 }
 
 func (s *Screen) JumpLines() {
